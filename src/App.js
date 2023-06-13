478c695b3a0f961bf29bd60325f9712f5e8c164b
Jakub <kozak.jakub@post.cz>
2023-06-13 16:02:38 +0200

    přidán dodatečný search bar na hledání podle herců

--- src/App.js
+++ src/App.js
@@ -9,20 +9,29 @@ import {useState} from "react";
 
 function App() {
     const [searchedWord, setSearchedWord] = useState("")
+    const [searchedActor, setSearchedActor] = useState("")
 
     function changeSearchedWord(inputValue) {
         setSearchedWord(inputValue)
+    }
+
+    function handleAdditionalSearch(actorsName) {
+        setSearchedActor(actorsName)
+        console.log("Hledaný herec: ", searchedActor)
     }
 
     console.log("-------search: ", searchedWord)
 
     return (
         <BrowserRouter>
-            <TopMenu changeSearchedWord={changeSearchedWord}/>
+            <TopMenu changeSearchedWord={changeSearchedWord} handleAdditionalSearch={handleAdditionalSearch}/>
 
 
             <Routes>
-                <Route exact path={"/"} element={<MainContent searchedWord={searchedWord}/>}/>
+                <Route exact path={"/"} element={<MainContent
+                    searchedWord={searchedWord}
+                    searchedActor={searchedActor}
+                />}/>
                 <Route path={"/film/:id"} element={<CardInfo/>}/>
                 <Route path={"/film/add"} element={<CardNew/>}/>
             </Routes>
--- src/components/TopMenu/TopMenu.js
+++ src/components/TopMenu/TopMenu.js
@@ -1,7 +1,11 @@
 import "./TopMenu.css";
 import {Link} from "react-router-dom";
+import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
+import {useState} from "react";
 
-function TopMenu({changeSearchedWord}) {
+function TopMenu({changeSearchedWord, handleAdditionalSearch}) {
+    const [showAdditionalSearchBar, setShowAdditionalSearchBar] = useState(true)
 
     const handleInputChange = (event) => {
         const inputValue = event.target.value
@@ -9,12 +13,38 @@ function TopMenu({changeSearchedWord}) {
         console.log("--slovo: ", event.target.value)
     }
 
+    const handleActorsChange = (event) => {
+        handleAdditionalSearch(event.target.value)
+        console.log("searched actor: ", event.target.value)
+    }
+
+    const changeAdditionalSearchBar = () => {
+        setShowAdditionalSearchBar(!showAdditionalSearchBar)
+        console.log("-------ZMENA", showAdditionalSearchBar)
+    }
+
+
     return (
-        <nav className={"TopNav"}>
-            <Link to={"/"}>Filmy</Link>
-            <Link to={"/film/add"}>Přidat film</Link>
-            <input id={"searchBar"} placeholder={"Vyhledat film"} onChange={handleInputChange}/>
-        </nav>
+        <>
+            <nav className={"TopNav"}>
+                <Link to={"/"}>Filmy</Link>
+                <Link to={"/film/add"}>Přidat film</Link>
+                <input id={"searchBar"} placeholder={"Vyhledat film"} onChange={handleInputChange}/>
+            </nav>
+            {showAdditionalSearchBar &&
+                <div className={"additionalSearchBar"}>
+                    <label>Vyhledat herce</label><input placeholder={"Jméno herce"} onChange={handleActorsChange}/>
+                </div>}
+            <div className={"halfCircle"}>
+                {showAdditionalSearchBar
+                    ? <span onClick={changeAdditionalSearchBar}>
+                        <CloseIcon id={"addSearchIcon"}
+                                   sx={{color: "white", fontSize: 30}}/></span>
+                    : <span onClick={changeAdditionalSearchBar}>
+                        <SearchIcon id={"addSearchIcon"}
+                                    sx={{color: "white", fontSize: 30}}/></span>}
+            </div>
+        </>
     )
 }
 
