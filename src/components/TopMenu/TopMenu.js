ff941688012995b84d24086fd8305aa5a4a7fde3
Jakub <kozak.jakub@post.cz>
2023-06-27 14:15:45 +0200

    rozpracováno

--- src/components/TopMenu/TopMenu.js
+++ src/components/TopMenu/TopMenu.js
@@ -1,7 +1,6 @@
 import "./TopMenu.css";
 import {Link} from "react-router-dom";
 import SearchIcon from '@mui/icons-material/Search';
-import CloseIcon from '@mui/icons-material/Close';
 import {useState} from "react";
 
 function TopMenu({changeSearchedWord, handleAdditionalSearch}) {
@@ -18,32 +17,31 @@ function TopMenu({changeSearchedWord, handleAdditionalSearch}) {
         console.log("searched actor: ", event.target.value)
     }
 
-    const changeAdditionalSearchBar = () => {
-        setShowAdditionalSearchBar(!showAdditionalSearchBar)
-        console.log("-------ZMENA", showAdditionalSearchBar)
-    }
-
 
     return (
         <>
-            <nav className={"TopNav"}>
-                <Link to={"/"}>Filmy</Link>
-                <Link to={"/film/add"}>Přidat film</Link>
-                <input id={"searchBar"} placeholder={"Vyhledat film"} onChange={handleInputChange}/>
-            </nav>
-            {showAdditionalSearchBar &&
-                <div className={"additionalSearchBar"}>
-                    <label>Vyhledat herce</label><input placeholder={"Jméno herce"} onChange={handleActorsChange}/>
-                </div>}
-            <div className={"halfCircle"}>
-                {showAdditionalSearchBar
-                    ? <span onClick={changeAdditionalSearchBar}>
-                        <CloseIcon id={"addSearchIcon"}
-                                   sx={{color: "white", fontSize: 30}}/></span>
-                    : <span onClick={changeAdditionalSearchBar}>
-                        <SearchIcon id={"addSearchIcon"}
-                                    sx={{color: "white", fontSize: 30}}/></span>}
+            <div className={"TopNav"}>
+                <div className={"logoBar"}>
+                    <div id={"LOGO"}><Link to={"/"}>Filmy.com</Link></div>
+                    <div className={"searchContainer"}>
+                        <input id={"searchBar"} placeholder={"Vyhledat film"} onChange={handleInputChange}/>
+                        <button id={"searchButton"}>
+                            <SearchIcon/>
+                        </button>
+                    </div>
+                    <div><Link to={"/filter"}>rozšířené vyhledávání</Link></div>
+                </div>
+                <nav>
+                    <Link to={"/"}>Filmy</Link>
+                    <Link to={"/film/add"}>Přidat film</Link>
+
+                </nav>
             </div>
+
+
+            {/*<input placeholder={"Jméno herce"} onChange={handleActorsChange}/>*/}
+
+
         </>
     )
 }
